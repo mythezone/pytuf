38d432876d376626c98780b15f7dfffcd7d2bfc2
mythezone <mythezone@gmail.com>
2024-09-06 00:16:10 +0800

    Refactor multi_pipeline function for better readability and maintainability

--- frontend/tools/movie_javdb.py
+++ frontend/tools/movie_javdb.py
@@ -16,11 +16,10 @@ from frontend.tools.process_json import process_json
 
 from frontend.tools.base import get_response_by_url, sessions, new_session, headers, save_resource_by_url  
 
-def multi_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"):   
+def multi_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"): 
     start_time = time.time()
     
     while True:
-            
         print("Start processing files in ", file_path)
         end_time = time.time()
         if end_time - start_time > 36000:
@@ -30,16 +29,16 @@ def multi_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"):
             for root, dirs, files in os.walk(file_path): 
                 print("Start processing files in ", root)
                 files = [x for x in files if x.endswith(".mp4") or x.endswith(".mkv")]
-                count = 0
+
                 for file in files:
-                    if file[0] in ["0","1","2","3","4","5","6","7","8","9","F"]:
+                    print("File:",file)
+                    if file[0] in ["0","1","2","3","4","5","6","7","8","9","F","P"]:
                         continue
                     
                     file_size = os.path.getsize(os.path.join(root,file))
                     if file_size > 500*1024*1024:
                         wait_time = single_pipeline(os.path.join(root,file),info_path)
-                    print(f"{count+1}/{len(files)}: Processing {file} done: next file in {wait_time} seconds")
-                    count+=1
+
                     if wait_time!= 0:
                         time.sleep(wait_time)
                         
@@ -50,6 +49,7 @@ def multi_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"):
             continue
 
 def get_info_path_by_code(code, info_path=r"\\10.16.12.105\disk\G\Info"):
+    
     first_letter = code[0]
     first_3_letters = code[:3]
     folder = os.path.join(info_path,first_letter,first_3_letters,code)
@@ -77,12 +77,17 @@ def single_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"):
             if json_dict.get("processed",False):
                 return 0
     else:
-        url = get_url_by_code(code)
+        href = get_href_by_code(code)
+        if href is None:
+            print(f"cant get url by code:{code}")
+            return 5
+        
         try:
-            json_dict = get_movie_json_by_url(url, html_file)
+            json_dict = get_movie_json_by_url(href=href,html_file=html_file)
             json_dict["video_path"] = file_path
         except Exception as e:
-            print(f"cant get movie json by url:{url}")
+            print(f"cant get movie json by url:{href}")
+
             print(e)
             return 10
     
@@ -96,9 +101,8 @@ def single_pipeline(file_path, info_path=r"\\10.16.12.105\disk\G\Info"):
         return 10
     
     return random.randint(10,20)
-        
 
-def get_url_by_code(code):
+def get_href_by_code(code):
     url = f"https://javdb.com/search?q={code}"
 
     try:
@@ -108,7 +112,17 @@ def get_url_by_code(code):
         print(f"Error in getting response from url:{url}")
         return None 
     
-    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    if response.status_code != 200:
+        print(f"Error in getting response from url:{url}")
+        return None
+    
+    text = getattr(response, "text",None)
+    
+    if text is None:
+        print(f"No text found in response from url:{url}")
+        return 
+
+    soup = bs4.BeautifulSoup(text, "html.parser")
     
     
     movie_list = soup.find_all("div", class_="item")
@@ -137,22 +151,40 @@ def get_movie_json_by_url(href=None,url=None, html_file=True):
             soup = bs4.BeautifulSoup(f.read(),"html.parser")
     else:
         response = get_response_by_url(url)
+    if response.status_code > 300:
+        print(f"Error in getting response from url:{url}")
+        return info
+    text = getattr(response, "text",None)
+    if text is None:
+        print(f"No text found in response from url:{url}")
+        return info
+    
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
         
-        
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
         
     video_detail = soup.find("div",{"class":"video-detail"})
+
     
     if video_detail is not None:
-        
-        code = video_detail.find("strong").text.strip()
-        info["code"] = code
-        info["folder"] = get_info_path_by_code(code)
-        with open(os.path.join(info["folder"],"javdb_"+info["code"]+".html"),"w",encoding="utf-8") as f:
-            f.write(response.text)
-        
-        info["current-title"]=video_detail.find("strong",{"class":"current-title"}).text
         try:
+            
+            code = video_detail.find("strong").text.strip()
+            info["code"] = code
+            info["folder"] = get_info_path_by_code(code)
+            with open(os.path.join(info["folder"],"javdb_"+info["code"]+".html"),"w",encoding="utf-8") as f:
+                f.write(response.text)
+        except Exception as e:
+            print(f"No code found:{info['id']}")
+            info["code"] = ""
+        
+        try:
+            info["current-title"]=video_detail.find("strong",{"class":"current-title"}).text
+        except Exception as e:
+            print(f"No current title found:{info['code']}")
+            info["current-title"] = ""
+        
+        try:
+            
             info["origin-title"]=video_detail.find("span",{"class":"origin-title"}).text
         except Exception as e:
             print(f"No origin title found:{info['code']}")
@@ -197,25 +229,26 @@ def get_movie_json_by_url(href=None,url=None, html_file=True):
         
     info["meta"] = panel
     
-
-            
     video = soup.find("video",{"id":"preview-video"})
     if video is not None:
         info['video_src'] = video.find("source")["src"]
     
     magnet_content = soup.find("div",{"id":"magnets-content"})
-    
-    if magnet_content is not None:
-        magnets = []
-        for magnet in magnet_content.find_all('div',{"class":"item"}):
-            href = magnet.find("a")["href"]
-            name = magnet.find("span",{"class":"name"}).text.strip()
-            meta = magnet.find("span",{"class":"meta"}).text.strip()
-            tags = [x.text.strip() for x in magnet.find_all("span",{"class":"tag"})]
-            time = magnet.find("span",{"class":"time"}).text.strip()
-            magnets.append({"href":href,"name":name,"meta":meta,"tags":tags,"time":time})
-        
-        info["magnets"] = magnets
+    try:
+        if magnet_content is not None:
+            magnets = []
+            for magnet in magnet_content.find_all('div',{"class":"item"}):
+                href = magnet.find("a")["href"]
+                name = magnet.find("span",{"class":"name"}).text.strip()
+                meta = magnet.find("span",{"class":"meta"}).text.strip()
+                tags = [x.text.strip() for x in magnet.find_all("span",{"class":"tag"})]
+                time = magnet.find("span",{"class":"time"}).text.strip()
+                magnets.append({"href":href,"name":name,"meta":meta,"tags":tags,"time":time})
+            
+            info["magnets"] = magnets
+    except Exception as e:
+        print(f"No magnets found:{info['code']}")
+        info["magnets"] = []
 
     try:
         cover_src = soup.find("img",{"class":"video-cover"})["src"]
@@ -267,43 +300,7 @@ def parse_relateds(related):
         
         return res 
         
-def parse_panel_info(panel_info):
-    
-    """
-    {'番號:': 'ABP-123', '日期:': '2014-04-01', '時長:': '140 分鍾', '導演:': 'Porn Stars', '片商:': 'プレステージ', '發行:': 'ABSOLUTELY PERFECT', '系列:': '満足度満点ソープ', '評分:': '\xa03.43分, 由45人評價', '類別:': '巨乳,\xa0單體作品,\xa0妓女,\xa0無碼破解', '演員:': '\nさつき芽衣♀\xa0\n        瀬田一花♀\xa0\n        月野かすみ♀\xa0\n        結城りの♀\xa0\n        美波もも♀\xa0\n        乃木蛍♀\xa0\n    '}
-    """
-    new_info = {}
-    for k,v in panel_info.items():
-        if k == "番號:":
-            new_info["code"] = v
-        elif k == "日期:":
-            new_info["release_date"] = v
-        elif k == "時長:":
-            new_info["duration"] = v
-        elif k == "導演:":
-            new_info["director"] = v
-        elif k == "片商:":
-            new_info["studio"] = v
-        elif k == "發行:":
-            new_info["publisher"] = v
-        elif k == "系列:":
-            new_info["series"] = v
-        elif k == "評分:":
-            new_info["rating"] = v
-        elif k == "類別:":
-            new_info["categories"] = v.split(",\xa0")
-        elif k == "演員:":
-            new_info["actress"] =[x.strip() for x in  v.split("\xa0\n") if x.strip() != "" and x.strip().endswith("♀")]
-            new_info["actor"] = [x.strip() for x in  v.split("\xa0\n") if x.strip() != "" and x.strip().endswith("♂")]
-            
-    pprint(new_info)
-    
-def download_image(image_url, file_path):
-    response = requests.get(image_url)
-    with open(file_path, 'wb') as f:
-        f.write(response.content)
-    return file_path
-    
+
 if __name__ == "__main__":
     # multi_pipeline(r"\\10.16.12.105\disk\G\Adult")
     multi_pipeline(r"\\10.16.12.105\disk\D\Adult")
@@ -311,9 +308,3 @@ if __name__ == "__main__":
     multi_pipeline(r"\\10.16.12.105\disk\media\4t\Adult")
     multi_pipeline(r"\\10.16.12.105\disk\media\4t2\Adult")
     multi_pipeline(r"\\10.16.12.105\disk\media\16t\Adult")
-    
-    # # parse_panel_info({'番號:': 'ABP-123', '日期:': '2014-04-01', '時長:': '140 分鍾', '導演:': 'Porn Stars', '片商:': 'プレステージ', '發行:': 'ABSOLUTELY PERFECT', '系列:': '満足度満点ソープ', '評分:': '\xa03.43分, 由45人評價', '類別:': '巨乳,\xa0單體作品,\xa0妓女,\xa0無碼破解', '演員:': '\nさつき芽衣♀\xa0\n        瀬田一花♀\xa0\n        月野かすみ♀\xa0\n        結城りの♀\xa0\n        美波もも♀\xa0\n        乃木蛍♀\xa0\n    '})
-    # images = ['https://c0.jdbstatic.com/samples/yx/yxxdwd_l_0.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_1.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_2.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_3.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_4.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_5.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_6.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_7.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_8.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_9.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_10.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_11.jpg', 'https://c0.jdbstatic.com/samples/yx/yxxdwd_l_12.jpg']
-    
-    # for i in range(len(images)):
-    #     download_image(images[i],f"static/test/image_{i}.jpg")
